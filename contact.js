b94e369fc46502b9011e3b167539e414fe859def
realfglez <realfglez@gmail.com>
2021-04-13 19:20:19 -0400

    Updated About and Contact pages

--- contact.js
+++ contact.js
@@ -3,17 +3,23 @@ var $ = function(id) {
 };
 
 var submitContact = function() {
+    var name = $("name").value;
     var email = $("email").value;
     var phone = parseInt($("phone").value);
     var valid = true;
 
+    if (!(name.toString().length > 0)) {
+        $("em1").firstChild.nodeValue = "Please enter a name.";
+        valid = false;
+    }
+
     if (!email.includes("@" && ".") || email == "") {
-        $("em1").firstChild.nodeValue = "Please enter a valid email address.";
+        $("em2").firstChild.nodeValue = "Please enter a valid email address.";
         valid = false;
     }
 
     if (isNaN(phone) || !(phone.toString().length > 9)) {
-        $("em2").firstChild.nodeValue = "Please enter a valid phone number.";
+        $("em3").firstChild.nodeValue = "Please enter a valid phone number.";
         valid = false;
     }
 
@@ -21,12 +27,14 @@ var submitContact = function() {
         window.location.href = "contact-success.html";
         $("em1").firstChild.nodeValue = "";
         $("em2").firstChild.nodeValue = "";
+        $("em3").firstChild.nodeValue = "";
     }
 };
 
 var clearPage = function (){
     $("em1").firstChild.nodeValue = "*";
     $("em2").firstChild.nodeValue = "*";
+    $("em3").firstChild.nodeValue = "*";
 };
 
 window.onload = function() {
