4f5819b0e6298f055cd5f05a1ee1a68316964720
realfglez <realfglez@gmail.com>
2021-04-18 18:41:56 -0400

    Updated validate-account.php, validate-signup.php, and validate-contact.php to include correct numbers-validate.php file

--- validate-account.php
+++ validate-account.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 require('database.php');
-include_once 'number_validation.php';
+include_once 'numbers-validate.php';
 $_SESSION['error'] = array();
 $_SESSION['success'] = array();
 
