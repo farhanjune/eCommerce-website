dc79890774477195e7e28ae86fb49dcaa5de2fcc
Nahiyaan-Sheikh <81336214+Nahiyaan-Sheikh@users.noreply.github.com>
2021-04-21 15:15:22 -0400

    Update cart_view.php

--- cart_view.php
+++ cart_view.php
@@ -49,7 +49,7 @@ $username = $_SESSION['username'];
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<title>BuyTech | Electronics</title>
-		<link rel="stylesheet" href="style.css">
+		<link rel="stylesheet" href="cartviewstyle.css">
 		<link rel="preconnect" href="https://fonts.gstatic.com">
 		<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
