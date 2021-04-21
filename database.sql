930496521074a464a8f39fe2f840a65778761959
Nahiyaan-Sheikh <81336214+Nahiyaan-Sheikh@users.noreply.github.com>
2021-04-21 12:38:51 -0400

    Added products to database

--- database.sql
+++ database.sql
@@ -12,6 +12,17 @@ CREATE TABLE `products` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 INSERT INTO `products` (`productID`, `categoryID`, `productName`, `listPrice`, `productImage`) VALUES ('1', '2', 'Airpods Pros', '249.49', 'images/product-2.jpeg');
 INSERT INTO `products` (`productID`, `categoryID`, `productName`, `listPrice`, `productImage`) VALUES ('2', '3', 'Apple Watch Series 5', '500.50', 'images/product-1.jpeg');
+INSERT INTO `products` (`productID`, `categoryID`, `productName`, `listPrice`, `productImage`) VALUES ('3', '4', 'Lenovo Yoga', '899.99', 'images/product-3.jpeg');
+INSERT INTO `products` (`productID`, `categoryID`, `productName`, `listPrice`, `productImage`) VALUES ('4', '1', 'Google Nest Audio', '79.99', 'images/product-4.jpeg');
+INSERT INTO `products` (`productID`, `categoryID`, `productName`, `listPrice`, `productImage`) VALUES ('5', '5', 'Samsung QLED Monitor', '669.99', 'images/product-5.jpeg');
+INSERT INTO `products` (`productID`, `categoryID`, `productName`, `listPrice`, `productImage`) VALUES ('6', '6', 'DualShock 4 Controller', '59.99', 'ps4.png');
+INSERT INTO `products` (`productID`, `categoryID`, `productName`, `listPrice`, `productImage`) VALUES ('7', '7', 'Fire TV Stick 4k', '49.99', 'firestick.jpeg');
+INSERT INTO `products` (`productID`, `categoryID`, `productName`, `listPrice`, `productImage`) VALUES ('8', '1', 'Amazon Echo Dot(4th gen)', '49.99', 'echodot.jpeg');
+INSERT INTO `products` (`productID`, `categoryID`, `productName`, `listPrice`, `productImage`) VALUES ('9', '8', 'GoPro - HERO9', '399.99', 'gopro.jpeg');
+INSERT INTO `products` (`productID`, `categoryID`, `productName`, `listPrice`, `productImage`) VALUES ('10', '1', 'Ring Video Doorbell', '99.99', 'ringbell.jpeg');
+INSERT INTO `products` (`productID`, `categoryID`, `productName`, `listPrice`, `productImage`) VALUES ('11', '4', 'Macbook Air 13.3" - M1 chip', '949.99', 'macbook.jpeg');
+INSERT INTO `products` (`productID`, `categoryID`, `productName`, `listPrice`, `productImage`) VALUES ('12', '4', 'iPad Pro 11"(256 GB)', '899.00', 'ipad.jpeg');
+
 CREATE TABLE `categories` (
   `categoryID` int NULL DEFAULT NULL,
   `categoryName` varchar(255) NULL DEFAULT NULL
