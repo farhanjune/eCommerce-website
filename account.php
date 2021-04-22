afa12ee01724cbe8a458a27514efc34e4c5e05f9
Sol <viratic@gmail.com>
2021-04-22 12:51:25 -0400

    fixed account.php's css

--- account.php
+++ account.php
@@ -31,7 +31,7 @@ else{
 		<script src="//code.jquery.com/jquery-1.10.2.js"></script>
         <meta charset="utf-8">
         <title>Account</title>
-        <link rel="stylesheet" href="login-form.css">
+        <link rel="stylesheet" href="account.css">
     </head>
     <body>
         <div class="header">
@@ -40,6 +40,7 @@ else{
 				<script>
 				$("#header").load("header.php");
 				</script>
+				<!---
                 <div class="row">
                     <div class="col-2">
                         <h1>Account Information</h1>
@@ -47,17 +48,32 @@ else{
                             Please update any information and click save.
                         </p>
                     </div>
-                    <div class="col-2">
+					
+                    <div class="image1">
                         <img src="images/image1.png">
                     </div>
+					
                 </div>
+				--->
             </div>
         </div>
-        <div class="login-box">
+        <div class="account-box">
+		<div class="small-container">
+			<!---
             <img src="images/circle_logo.png" class="avatar" alt="Avatar Image">
+			--->
+			<div class="row">
+                    <div class="col-2">
+                        <h1>Account Information</h1>
+                        <p>
+                            Please update any information and click save.
+                        </p>
+                    </div>
+					
+                </div>
             <form action="validate-account.php" method="post">
                 <label for="name">Name</label>
-                <input id="name" name="name" type="text" value="<?php echo $name; ?>">
+                <input id="name" name="name" type="text" value="<?php echo $name; ?>"><br>
                 <?php
                 if(isset($_SESSION['error']['name_error'])){
                     $error = $_SESSION['error']['name_error'];
@@ -65,7 +81,7 @@ else{
                 }
                 ?>
                 <label for="email">Email</label>
-                <input id="email" name="email" type="text" value="<?php echo $email; ?>">
+                <input id="email" name="email" type="text" value="<?php echo $email; ?>"><br>
                 <?php
                 if(isset($_SESSION['error']['email_error'])){
                     $error = $_SESSION['error']['email_error'];
@@ -73,7 +89,7 @@ else{
                 }
                 ?>
                 <label for="phone">Phone number</label>
-                <input id="phone" name="phone" type="text" value="<?php echo $phone; ?>">
+                <input id="phone" name="phone" type="text" value="<?php echo $phone; ?>"><br>
                 <?php
                 if(isset($_SESSION['error']['phone_error'])){
                     $error = $_SESSION['error']['phone_error'];
@@ -81,7 +97,7 @@ else{
                 }
                 ?>
                 <label for="username">Username</label>
-                <input id="username" name="username" type="text" value="<?php echo $username; ?>">
+                <input id="username" name="username" type="text" value="<?php echo $username; ?>"><br>
                 <?php
                 if(isset($_SESSION['error']['username_error'])){
                     $error = $_SESSION['error']['username_error'];
@@ -93,9 +109,9 @@ else{
                     <option value="visa" <?php if($card_type == 'visa'){echo 'selected';} ?>>Visa</option>
                     <option value="mastercard" <?php if($card_type == 'mastercard'){echo 'selected';} ?>>MasterCard</option>
                     <option value="discover"<?php if($card_type == 'discover'){echo 'selected';} ?>>Discover</option>
-                </select>
+                </select><br>
                 <label for="card_number">Card number</label>
-                <input id="card_number" name="card_number" autocomplete="new-password" type="password" placeholder="New or leave blank">
+                <input id="card_number" name="card_number" autocomplete="new-password" type="password" placeholder="New or leave blank"><br>
                 <?php
                 if(isset($_SESSION['error']['card_number_error'])){
                     $error = $_SESSION['error']['card_number_error'];
@@ -103,7 +119,7 @@ else{
                 }
                 ?>
                 <label for="card_security">Security code</label>
-                <input id="card_security" name="card_security" type="password" autocomplete="off" placeholder="New or leave blank">
+                <input id="card_security" name="card_security" type="password" autocomplete="off" placeholder="New or leave blank"><br>
                 <?php
                 if(isset($_SESSION['error']['card_security_error'])){
                     $error = $_SESSION['error']['card_security_error'];
@@ -111,7 +127,7 @@ else{
                 }
                 ?>
                 <label for="street">Street address</label>
-                <input id="street" name="street" type="text" value="<?php if (isset($street)){ echo $street;} ?>">
+                <input id="street" name="street" type="text" value="<?php if (isset($street)){ echo $street;} ?>"><br>
                 <?php
                 if(isset($_SESSION['error']['street_error'])){
                     $error = $_SESSION['error']['street_error'];
@@ -119,7 +135,7 @@ else{
                 }
                 ?>
                 <label for="city">City</label>
-                <input id="city" name="city" type="text" value="<?php if (isset($city)){ echo $city;} ?>">
+                <input id="city" name="city" type="text" value="<?php if (isset($city)){ echo $city;} ?>"><br>
                 <?php
                 if(isset($_SESSION['error']['city_error'])){
                     $error = $_SESSION['error']['city_error'];
@@ -127,7 +143,7 @@ else{
                 }
                 ?>
                 <label for="state">State</label>
-                <input id="state" name="state" type="text" value="<?php if (isset($state)){ echo $state;} ?>">
+                <input id="state" name="state" type="text" value="<?php if (isset($state)){ echo $state;} ?>"><br>
                 <?php
                 if(isset($_SESSION['error']['state_error'])){
                     $error = $_SESSION['error']['state_error'];
@@ -135,7 +151,7 @@ else{
                 }
                 ?>
                 <label for="zip">Zip/postal code</label>
-                <input id="zip" name="zip" type="number" value="<?php if (isset($zip)){ echo $zip;} ?>">
+                <input id="zip" name="zip" type="number" value="<?php if (isset($zip)){ echo $zip;} ?>"><br>
                 <?php
                 if(isset($_SESSION['error']['zip_error'])){
                     $error = $_SESSION['error']['zip_error'];
@@ -143,11 +159,13 @@ else{
                 }
                 ?>
                 <input type="submit" value="Save">
-                <a href="change-password.php">Change password</a><br>
+                <a href="change-password.php">Change password</a><br><br>
             </form>
+		</div>
+		
         </div>
         <div class="footer">
-        <div class="container">
+		<div class="container">
             <div class="row">
                 <div class="footer-col-1">
                     <h3>Links</h3>
@@ -170,10 +188,22 @@ else{
                     </ul>
                 </div>
             </div>
-                <hr>
-                <p class="copyright">Copyright 2021 - BuyTech</p>
-            </div>
-        </div>
+				<p class="copyright">Copyright 2021 - BuyTech</p>
+			</div>
+		</div>
+		<script>
+			var menu_items = document.getElementById('menu-items');
+
+			menu_items.style.maxHeight = "0px";
+
+			function menutoggle() {
+				if(menu_items.style.maxHeight == "0px"){
+					menu_items.style.maxHeight = "200px";
+				}else{
+					menu_items.style.maxHeight = "0px";
+				}
+			}
+		</script>
     </body>
 </html>
 
