a5c8f9c500840791e74c4d99ef580733e666f920
Nahiyaan-Sheikh <81336214+Nahiyaan-Sheikh@users.noreply.github.com>
2021-04-25 23:07:59 -0400

    added product images and desc

--- database.sql
+++ database.sql
@@ -59,17 +59,54 @@ Convenient 15.6 touchscreen with stunning visuals, 4K VESA display
 Redesigned rotating soundbar and 2 additional speakers make everything from meetings to your favorite music crystal clear
 Enhanced webcam security with TrueBlock Privacy Shutte');
 
-INSERT INTO `products` (`productID`, `categoryID`, `productName`, `listPrice`, `productImage`)  VALUES ('4', '1', 'Google Nest Audio', '79.99', 'images/product-4.jpeg');
+INSERT INTO `products` (`productID`, `categoryID`, `productName`, `listPrice`, `productImage`, `productImage2`, `productImage3`, `description`)  
+VALUES ('4', '1', 'Google Nest Audio', '79.99', 'images/product-4.jpeg', 'images/googlenest1.jpg', 'images/goognest2.jpg', 'Meet Nest Audio. Hear music the way it should sound, with crisp vocals and powerful bass that fill the room. Just say, Hey Google to play music or get help.
+Features Designed to protect your privacy. Nest Audio comes with privacy built in. You can delete your Assistant history by saying, Hey Google, delete what I just said. Or to turn off the mic, just use the switch on the back.
+Its all about sound. Just say, Hey Google, play some music, and crisp vocals and powerful bass fill the room. Nest Audio adapts to your environment and whatever you are listening to, so music sounds better.
+Music here. Music there. Music everywhere. Create a home audio system that fills your home with sound.* Nest Audio works together with your other Nest speakers and displays, Chromecast-enabled devices, or compatible speakers.
+Connect with family and friends. Use your Nest speakers as an intercom and chat from room to room. And make audio calls with Duo');
 
-INSERT INTO `products` (`productID`, `categoryID`, `productName`, `listPrice`, `productImage`)  VALUES ('5', '5', 'Samsung QLED Monitor', '669.99', 'images/product-5.jpeg');
-INSERT INTO `products` (`productID`, `categoryID`, `productName`, `listPrice`, `productImage`)  VALUES ('6', '6', 'DualShock 4 Controller', '59.99', 'images/ps4.png');
-INSERT INTO `products` (`productID`, `categoryID`, `productName`, `listPrice`, `productImage`)  VALUES ('7', '7', 'Fire TV Stick 4k', '49.99', 'images/firestick.jpeg');
-INSERT INTO `products` (`productID`, `categoryID`, `productName`, `listPrice`, `productImage`)  VALUES ('8', '1', 'Amazon Echo Dot(4th gen)', '49.99', 'images/echodot.jpeg');
-INSERT INTO `products` (`productID`, `categoryID`, `productName`, `listPrice`, `productImage`)  VALUES ('9', '8', 'GoPro - HERO9', '399.99', 'images/gopro.jpeg');
-INSERT INTO `products` (`productID`, `categoryID`, `productName`, `listPrice`, `productImage`)  VALUES ('10', '1', 'Ring Video Doorbell', '99.99', 'images/ringbell.jpeg');
-INSERT INTO `products` (`productID`, `categoryID`, `productName`, `listPrice`, `productImage`)  VALUES ('11', '4', 'Macbook Air 13.3" - M1 chip', '949.99', 'images/macbook.jpeg');
-INSERT INTO `products` (`productID`, `categoryID`, `productName`, `listPrice`, `productImage`)  VALUES ('12', '4', 'iPad Pro 11"(256 GB)', '899.00', 'images/ipad.jpeg');
+INSERT INTO `products` (`productID`, `categoryID`, `productName`, `listPrice`, `productImage`, `productImage2`, `productImage3`, `description`)  
+VALUES ('5', '5', 'Samsung QLED Monitor', '669.99', 'images/product-5.jpeg', 'images/qled1.jpg', 'images/qled2.jpg', 'Enhance your viewing experience with this 34-inch Samsung Thunderbolt monitor. Its 21:9 aspect ratio lets you multitask by browsing various windows and applications side-by-side, and the curved design reduces eyestrain while providing maximum immersion. This Samsung Thunderbolt monitor uses AMD FreeSync technology to deliver smooth frames for gaming and video playback.
+3440 x 1440 resolution with 21:9 aspect ratio Delivers more contents on the screen for easier multitasking.
+4 ms response time Ensures solid performance for precise gaming and video work. 100Hz refresh rate Provides smoother, silkier mouse sensitivity for enhanced productivity in any application.
+34" LED monitor Ensures an accurate and lifelike picture.');
 
+INSERT INTO `products` (`productID`, `categoryID`, `productName`, `listPrice`, `productImage`, `productImage2`, `productImage3`, `description`)  
+VALUES ('6', '6', 'DualShock 4 Controller', '59.99', 'images/ps4.png', 'images/dualshock41.jpg', 'images/dualshock42.jpg', 'Play your favorite video games with this PlayStation 4 DualShock controller. The microUSB port lets you keep it charged and ready to use, and the trigger buttons and controls deliver immediate response on even the most challenging games. This PlayStation 4 DualShock controller has an internal speaker for realistic close-up sound.');
+
+INSERT INTO `products` (`productID`, `categoryID`, `productName`, `listPrice`, `productImage`, `productImage2`, `productImage3`, `description`)  
+VALUES ('7', '7', 'Fire TV Stick 4k', '49.99', 'images/firestick.jpeg', 'images/firestick1.jpg', 'images/firestick2.jpg', 'With more power, a lightning-fast processor, support for 802.11 ac Wi-Fi, and a new antenna design, Fire TV Stick 4K allows you to enjoy a more complete 4K Ultra HD streaming experience. Launch and control all your favorite movies and TV shows with the next-gen Alexa Voice Remote. New power, volume, and mute buttons let you control your TV, sound bar, and receiver.');
+
+INSERT INTO `products` (`productID`, `categoryID`, `productName`, `listPrice`, `productImage`, `productImage2`, `productImage3`, `description`)  
+VALUES ('8', '1', 'Amazon Echo Dot(4th gen)', '49.99', 'images/echodot.jpeg', 'images/echodot1.jpg', 'images/echodot2.jpg', 'Meet the all-new Echo Dot with clock. Round out any room with Alexa. Our most popular smart speaker has a sleek, compact design that fits perfectly into small spaces. It delivers crisp vocals and balanced bass for full sound you can enjoy anywhere in your home. See the time, alarms, and timers on the LED display. Plus, tap the top to snooze an alarm.
+');
+
+INSERT INTO `products` (`productID`, `categoryID`, `productName`, `listPrice`, `productImage`, `productImage2`, `productImage3`, `description`)  
+VALUES ('9', '8', 'GoPro - HERO9', '399.99', 'images/gopro.jpeg', 'images/gopro1.jpg', 'images/gopro2.jpg', 'Record captivating vlogs and take brilliant photos with this black GoPro HERO9 camera. The high-quality CMOS sensor captures 5K video and up to 20.0MP images for stunning clarity, and support for a microSD card offers customizable storage space. This water-resistant GoPro HERO9 camera allows for use at the beach or pool.
+');
+
+INSERT INTO `products` (`productID`, `categoryID`, `productName`, `listPrice`, `productImage`, `productImage2`, `productImage3`, `description`)  
+VALUES ('10', '1', 'Ring Video Doorbell', '99.99', 'images/ringbell.jpeg', 'images/ring.jpg', 'images/ring2.jpg', '
+With 1080p HD video and Two-Way Talk, see, hear and speak to people from your phone, tablet or select Alexa-enabled device.
+Customize your motion zone settings to focus on areas you need to protect.
+Runs on a built-in, rechargeable battery
+Receive real-time mobile notifications when someone is at your door.
+Monitor what matters most with infrared technology—even when it’s dark.');
+
+INSERT INTO `products` (`productID`, `categoryID`, `productName`, `listPrice`, `productImage`, `productImage2`, `productImage3`, `description`)  
+VALUES ('11', '4', 'Macbook Air 13.3" - M1 chip', '949.99', 'images/macbook.jpeg', 'images/macbook1.jpg', 'images/macbook2.jpg', 'Apple’s thinnest and lightest notebook gets supercharged with the Apple M1 chip. Tackle your projects with the blazing-fast 8-core CPU. Take graphics-intensive apps and games to the next level with the 7-core GPU. And accelerate machine learning tasks with the 16-core Neural Engine. All with a silent, fanless design and the longest battery life ever — up to 18 hours.¹ MacBook Air. Still perfectly portable. Just a lot more powerful.');
+
+INSERT INTO `products` (`productID`, `categoryID`, `productName`, `listPrice`, `productImage`, `productImage2`, `productImage3`, `description`)  
+VALUES ('12', '4', 'iPad Pro 11"(256 GB)', '899.00', 'images/ipad.jpeg', 'images/ipad1.jpg', 'images/ipad2.jpg', 'iPad Pro features the powerful Apple M1 chip with next-level performance and all-day battery life.³ The Liquid Retina display on the 11-inch iPad Pro is not only gorgeous, but super portable.¹ And a front camera with Center Stage keeps you in frame automatically during video calls. iPad Pro has pro cameras and a LiDAR Scanner for stunning photos, videos, and immersive AR. Thunderbolt for connecting to high-performance accessories.
+
+Apple M1 chip for next-level performance
+
+Stunning 11-inch Liquid Retina display¹ with ProMotion, True Tone, and P3 wide color
+
+TrueDepth camera system featuring Ultra Wide front camera with Center Stage
+
+12MP Wide camera, 10MP Ultra Wide camera, and LiDAR Scanner for immersive AR');
 CREATE TABLE `categories` (
                               `categoryID` int NULL DEFAULT NULL,
                               `categoryName` varchar(255) NULL DEFAULT NULL
